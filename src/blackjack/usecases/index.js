2b29fedc0c19ace757ee97127d6f509cba82f7fc
Fernando Nicolás <fersanti2896@gmail.com>
2022-12-28 00:26:51 -0600

    Factorizando funcion de turno de la computadora en turn-computer.js

--- src/blackjack/usecases/index.js
+++ src/blackjack/usecases/index.js
@@ -1,3 +1,4 @@
 export { crearDeck } from './create-deck'
 export { pedirCarta } from './order-card';
 export { valorCarta } from './value-card';
+export { turnoComputadora } from './turn-computer' 
